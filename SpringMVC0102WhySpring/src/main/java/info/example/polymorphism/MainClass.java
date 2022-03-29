3752a318c405287bb1de83f1720fb062a9cf3839
Elogio.mi <Elogio.mi@DESKTOP-1BC77RC>
2022-03-30 03:05:36 +0900

    수정

--- SpringMVC0102WhySpring/src/main/java/info/example/polymorphism/MainClass.java
+++ SpringMVC0102WhySpring/src/main/java/info/example/polymorphism/MainClass.java
@@ -4,7 +4,7 @@ public class MainClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-			
+			//Class타입 Class명 = new 생성자()
 			YourPhone phone1 = new YourPhoneIPhone();
 			yourPhone(phone1);
 			YourPhone phone2 = new YourPhoneGalaxy();
