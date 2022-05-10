549efaaecbb98c1051cf3da30f334bb3d9c2b0db
elogio.mi <elogio.mi@gmail.com>
2022-05-10 11:25:37 +0900

    10th week

--- Spring0401UrlMappingExample/src/main/java/info/example/controller/HomeController.java
+++ Spring0401UrlMappingExample/src/main/java/info/example/controller/HomeController.java
@@ -11,9 +11,31 @@ public class HomeController {
 	public String home() {
 		return "index";
 	}
-	@RequestMapping(value = "/sub1/", method = RequestMethod.GET)
-	public String sub1() {
-		return "index";
+	
+	
+	@RequestMapping(value = "/test1", method = RequestMethod.GET)
+	public String test1() {
+		return "test1";
+	}
+	@RequestMapping(value = "/test2", method = RequestMethod.GET)
+	public String test2() {
+		return "test2";
+	}
+	@RequestMapping(value = "/sub1/test3", method = RequestMethod.GET)
+	public String test3() {
+		return "sub1/test3";
+	}
+	@RequestMapping(value = "/sub1/test4", method = RequestMethod.GET)
+	public String test4() {
+		return "sub1/test4";
+	}
+	@RequestMapping(value = "/sub2/test5", method = RequestMethod.GET)
+	public String test5() {
+		return "sub2/test5";
+	}
+	@RequestMapping(value = "/sub2/test6", method = RequestMethod.GET)
+	public String test6() {
+		return "sub2/test6";
 	}
 }
 
--- Spring0401UrlMappingExample/src/main/webapp/WEB-INF/views/index.jsp
+++ Spring0401UrlMappingExample/src/main/webapp/WEB-INF/views/index.jsp
@@ -7,11 +7,11 @@
 <title>index</title>
 </head>
 <body>
-<a href ="test1"> test1(문지현)</a><br/>
+<a href ="test1"> test1</a><br/>
 <a href ="test2"> test2</a><br/>
 <a href ="sub1/test3">sub1/test3</a><br/>
 <a href ="sub1/test4"> sub1/test4</a><br/>
-<a href ="sub1/test5"> sub1/test5</a><br/>
-<a href ="sub1/test6"> sub1/test6</a><br/>
+<a href ="sub2/test5"> sub2/test5</a><br/>
+<a href ="sub2/test6"> sub2/test6</a><br/>
 </body>
 </html>
